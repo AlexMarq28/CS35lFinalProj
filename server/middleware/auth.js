73e59c205973a175b76d4c7a62d08c2e28caaebc
onzerem <ojnzerem@gmail.com>
2021-05-23 16:23:56 -0700

    added server side of user login

---
+++ server/middleware/auth.js
@@ -0,0 +1,31 @@
+// checks to make sure user is allowed to do some action/checks user is legit (i.e edit, delete, like, etc)
+// when implement user actions, we'll need to include this later in the routes for the actions
+
+import jwt from "jsonwebtoken";
+
+const secret = 'test';
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
+
+    let decodedData;
+
+    if (token && isCustomAuth) {      
+      decodedData = jwt.verify(token, secret);
+
+      req.userId = decodedData?.id;
+    } else { // for google logins if we want to implement later
+      decodedData = jwt.decode(token);
+
+      req.userId = decodedData?.sub;
+    }    
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
