a447a114b4dc4727104d8ae1d25c9737a2951a6a
Colin Johnson <colinjohnson@MacBook-Pro-2.lan>
2021-05-24 15:51:42 -0700

    More changes trying to get heroku working

--- index.js
+++
@@ -1,9 +0,0 @@
-// Serve static assets (from index.js in client) if in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-  //load the index.html file
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
