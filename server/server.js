0c38cbbb9d1c7e84170069b274732a95d008ca3b
Colin Johnson <colinjohnson@MacBook-Pro-2.lan>
2021-05-24 16:40:23 -0700

    Heroku attempt 3

--- server/server.js
+++ server/server.js
@@ -14,10 +14,7 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB, use mongoose
 mongoose
-  .connect(process.env.mongoURI || db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected...")) //if you connect then...
   .catch((err) => console.log(err));
 
