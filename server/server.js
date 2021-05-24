2820fc7369f17cf2a112127fad14d51df61cbb23
Colin Johnson <colinjohnson@MacBook-Pro-2.lan>
2021-05-24 15:23:51 -0700

    Fixing small bug with Heroku not detecting buildpack

--- server/server.js
+++ server/server.js
@@ -34,10 +34,10 @@ if (process.env.NODE_ENV === "production") {
 // Serve static assets (from index.js in client) if in production
 if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
   //load the index.html file
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
