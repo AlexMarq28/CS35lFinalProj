b4b85745683549586642aef59a8300cb22fedbe4
onzerem <ojnzerem@gmail.com>
2021-05-26 18:26:44 -0700

    added stuff to userschema

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,10 @@ const userSchema = mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true },
     password: { type: String, required: true},
+    selectedFile: selectedFile,
+    bio: String,
+    categories: String,
     id: { type: String }
-})
+});
 
 export default mongoose.model("User", userSchema);
