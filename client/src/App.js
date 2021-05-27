9f3db485721e308ec5468033ff4cc7acf7117e4d
ernest0903 <84247241+ernest0903@users.noreply.github.com>
2021-05-27 12:06:54 -0700

    Imported 'Page' modules to App.js as a comment.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,23 @@ import Exerciselist from "./Components/ExerciseList";
 import ItemModal from "./Components/ItemModal";
 import { Container } from "reactstrap";
 
+/* Ernest (11:15 AM, 5/27/21)
+Description: First attempt at a commit.
+I've commented out the following code so nothing need blow up in case something goes wrong with it.
+The import statements listed below are to import all the pages from 'Pages'. 
+If I have it correct, the aim is to have the 'App' function use client-side routing to access the various pages.
+All we need to do is to set up all the links in the 'App' function to jump to each page.
+After we set that up, we can work on how we want each page to look.
+Link: https://www.youtube.com/watch?v=yQf1KbGiwiI
+
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+import Home from './Pages/Home';
+import LandingPage from './Pages/LandingPage';
+import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
+import SearchPage from './Pages/SearchPage';
+*/
+
 import { Provider } from "react-redux"; //package that binds together react & redux
 import store from "./store";
 
