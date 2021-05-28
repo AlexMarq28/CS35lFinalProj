60ba6c07b142769d387e57910cd3f6ff6b605d79
Colin Johnson <colinjohnson@MacBook-Pro-2.lan>
2021-05-28 13:06:33 -0700

    Getting a working basic model

--- server/server.js
+++ server/server.js
@@ -21,16 +21,15 @@ mongoose
 // Use Routes, anything that goes to /api/items should refer to items variable (the items file)
 app.use("/api/items", items);
 
-// // Serve static assets (would be build folder) if in production
-// if (process.env.NODE_ENV === "production") {
-//   //Set static folder
-//   //All the javascript and css files will be read and served from this folder
-//   app.use(express.static("client/build"));
-//   //Any request we get except /api/items should load up html
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-//   });
-// }
+// Serve static assets (would be build folder) if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  //Any request we get except /api/items should load up html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Creating variable for the port, env.PORT is an environmental variable
 const port = process.env.PORT || 5000;
@@ -40,29 +39,3 @@ const port = process.env.PORT || 5000;
 
 //listen on the port
 app.listen(port, () => console.log(`Server started on port: ${port}`));
-
-// ---------------------------------------------
-
-// import bodyParser from 'body-parser';
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import cors from 'cors';
-
-// import userRoutes from './routes/users.js'
-
-// const app = express();
-
-// app.use(express.json({ limit: "30mb", extended: true }));
-// app.use(express.urlencoded({ limit: "30mb", extended: true }));
-// app.use(cors());
-
-// app.use('/user', userRoutes);
-
-// const CONNECTION_URL = 'mongodb+srv://onzerem:onzerem123@cluster0.ixo3s.mongodb.net/cs35lproj?retryWrites=true&w=majority';
-// const PORT = process.env.PORT || 4000;
-
-// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-//     .catch((error) => console.log(error.message));
-
-// mongoose.set('useFindAndModify', false);
