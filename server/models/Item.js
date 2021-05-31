90523c917fe5a143a8f17565409590f9cbbba450
AlexMarq28 <alexmarq28@gmail.com>
2021-05-31 15:48:56 -0700

    Item Schema update with new data fields
    Add functionality for likes, times worked out, pictures, and tag-based search

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,3 +1,5 @@
+
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -11,9 +13,35 @@ const ItemSchema = new Schema({
     type: Date,
     default: Date.now,
   },
-  email: {
+   email: {
     type: String,
   },
-});
+   author: {
+    type: String,
+    required: true,
+   description: {
+    type: String,
+    required: true,
+  },
+   likes: {
+       type: Integer,
+       default: 0,
+   },
+   numSessions: {
+       type: Integer,
+       default: 0,
+   },
+     picture: {
+       type: String,
+       required: true,
+     },
+     tags: [{
+	 type: String
+     }],
+  
+     
+   });
+   
+
 
 module.exports = Item = mongoose.model("item", ItemSchema);
