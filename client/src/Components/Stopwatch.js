63fe52e4537e8cc5de26f256f21728bbb662ba0e
Colin Johnson <colinjohnson@MacBook-Pro-2.lan>
2021-06-01 09:21:22 -0700

    Added itemmodal and exerciselist to activity page, styling is bad though

--- client/src/Components/Stopwatch.js
+++ client/src/Components/Stopwatch.js
@@ -1,46 +1,46 @@
 import React from "react";
 import "./Stopwatch.css";
+import ExerciseList from "../Components/ExerciseList";
+import ItemModal from "../Components/ItemModal";
 
 function Stopwatch() {
-    const [time, setTime] = React.useState(0);
-    const [timerOn, setTimeOn] = React.useState(false);
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimeOn] = React.useState(false);
 
-    React.useEffect(() => {
-        let interval = null;
+  React.useEffect(() => {
+    let interval = null;
 
-        if (timerOn) { 
-            interval = setInterval(() => {
-                setTime(prevTime => prevTime + 10)
-            }, 10)
-        } else {
-            clearInterval(interval)
-        }
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
 
-        return () => clearInterval(interval)
-
-    }, [timerOn]);
-    return (
-        <div className="Stopwatch" >
-            <div className="digits" style={{fontSize: 72}}>
-                <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-                <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
-            </div>
-            <div className="font-link3" style={{fontSize: 36}}>
-                {!timerOn && time===0 && (
-                    <button onClick={() => setTimeOn(true)}>START</button>
-                )}
-                {timerOn && 
-                    <button onClick={() => setTimeOn(false)}>STOP</button>}
-                {!timerOn && time > 0 && (
-                    <button onClick={() => setTimeOn(true)}>RESUME</button>
-                )}
-                {!timerOn && time > 0 && (
-                    <button onClick={() => setTime(0)}>RESET</button>
-                )}
-            </div>
-        </div>
-    )
+    return () => clearInterval(interval);
+  }, [timerOn]);
+  return (
+    <div className="Stopwatch">
+      <div className="digits" style={{ fontSize: 72 }}>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div className="font-link3" style={{ fontSize: 36 }}>
+        {!timerOn && time === 0 && (
+          <button onClick={() => setTimeOn(true)}>START</button>
+        )}
+        {timerOn && <button onClick={() => setTimeOn(false)}>STOP</button>}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTimeOn(true)}>RESUME</button>
+        )}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTime(0)}>RESET</button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Stopwatch;
--- client/src/Pages/StopwatchPage.js
+++ client/src/Pages/StopwatchPage.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Provider } from "react-redux"; //package that binds together react & redux
 import store from "../store";
 import AppNavbar from "../Components/AppNavbar";
-import Exerciselist from "../Components/ExerciseList";
-import ItemModal from "../Components/ItemModal";
+// import Exerciselist from "../Components/ExerciseList";
+// import ItemModal from "../Components/ItemModal";
 import { Container, Row } from "reactstrap";
 import "./StopwatchPage.css";
-import SearchBar from "../Components/SearchBar";
 
 import Stopwatch from "../Components/Stopwatch.js";
+import ItemModal from "../Components/ItemModal";
+import ExerciseList from "../Components/ExerciseList";
 
 export default function StopwatchPage() {
   return (
@@ -19,14 +20,18 @@ export default function StopwatchPage() {
           <div className="font-link3">
             <h1
               className="StopwatchPageMessage"
-              style={{ color: "black", fontSize: 72 }}
+              style={{ color: "black", fontSize: 40 }}
             >
-              STOPWATCH OF DEATH
+              STOPWATCH
             </h1>
           </div>
           <Container>
             <Stopwatch />
           </Container>
+          <div>
+            <ItemModal />
+            <ExerciseList />
+          </div>
         </div>
       </div>
     </Provider>
