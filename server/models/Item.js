328179bde5fbfd15288a862bf1469f49b8864273
AlexMarq28 <alexmarq28@gmail.com>
2021-06-02 06:11:25 -0700

    item model updated with fields for more functionality, search, likes, photos

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,16 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-//Creating a schema
 const ItemSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  date: {
     type: Date,
     default: Date.now,
   },
+  email: {
+   email: {
+    type: String,
+  },
+   author: {
+    type: String,
+    required: true,
+   description: {
+    type: String,
+    required: true,
+  },
 });
+   likes: {
+       type: Integer,
+       default: 0,
+   },
+   numSessions: {
+       type: Integer,
+       default: 0,
+   },
+     picture: {
+       type: String,
+       required: true,
+     },
+     tags: [{
+	 type: String
+     }],
+
+
+   });
+
+
 
 module.exports = Item = mongoose.model("item", ItemSchema);
